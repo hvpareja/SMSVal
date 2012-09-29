f4ef9d667965a1ee098fe47708ff6431305c6d0f
Héctor Valverde <hvpareja@hotmail.com>
2012-09-29 15:29:11 +0200

    Validating variables
    Sheduled …

--- sms.pl
+++ sms.pl
@@ -55,18 +55,79 @@
 # Chapter 2: ----------------------------------------------------------
 # Variable assignation ------------------------------------------------
 # Assign variable values from config file or arguments
+# 2.1  Take params from config and or arguments
+    my $choosen_option = "";
+# 2.2. Build the complete url depending on the option choosen
+    my $url = $base_url.$opts_url{$choosen_option};
 # ---------------------------------------------------------------------
 
 # Chapter 3: ----------------------------------------------------------
 # Variable validation -------------------------------------------------
-#   1. Is $login not empty?
-#   2. Is $password not empty?
-#   3. Is $message_type not empty and well formed (GP or GS)?
-#   4. Is $message length greater than 1 and lower than 1000?
-#   5. Is $recipient a valid phone number?
-#   6. Is $sender well formed (phone or short varchar (11))?
-#   7. If $sheduled_delivery_time is not empty, it is well formated?
-#   8. Is $order_id length lower than 32 char?
+# $error = 0 if no error and 1 if error
+    my $error = 0;
+    my $error_message = "";
+#   1. Is $choosen_option between the allowed options?
+    if($opts_url{$choosen_option} eq ""){
+        $error_message.="Invalid option \"$choosen_option\"\n";
+        $error = 1;
+    }
+#   2. Is $login not empty?
+    if($login eq ""){
+        $error_message.="Not username defined\n";
+        $error = 1;
+    }
+#   3. Is $password not empty?
+    if($password eq ""){
+        $error_message.="No password defined\n";
+        $error = 1;
+    }
+#   4. Is $message_type not empty and well formed (GP or GS)?
+    $message_type = uc($message_type); # Turn in upper case
+    if($message_type ne "GP" || $message_type ne "GS"){
+        $error_message.="Invalid message type:\"$message_type\"\n";
+        $error = 1;
+    }
+#   5. Is $message length greater than 1 and lower than 1000?
+    my $msg_length = length($message);
+    if($msg_length > 1000){
+        $error_message.="Too many characters in the message\n";
+        $error = 1;
+    }
+    if($msg_length eq ""){
+        $error_message.="Message not defined\n";
+        $error = 1;
+    }
+#   6. Is $recipient a valid phone number?
+    if(!grep(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,$recipient)){
+        $error_message.="Invalid recipient: \"$recipient\"\n";
+        $error = 1;
+    }
+#   7. Is $sender well formed (phone or short varchar (11))?
+#   8. If $sheduled_delivery_time is not empty, it is well formated?
+    if($sheduled_delivery_time ne ""){
+        if(grep(/^2[0-9][0-9][0-9][0-1][0-9][0-3][0-9][0-2][0-9][0-6][0-9][0-6][0-9]$/,$sheduled_delivery_time)){
+            my $year = substr($sheduled_delivery_time,0,4);
+            my $month = substr($sheduled_delivery_time,0,4);
+            my $day = substr($sheduled_delivery_time,0,4);
+            my $hour = substr($sheduled_delivery_time,0,4);
+            my $minute = substr($sheduled_delivery_time,0,4);
+            my $second = substr($sheduled_delivery_time,0,4);
+        }else{
+            $error_message.="Sheduled delivery time bad formed (must be: yyyyMMddHHmmss).\n";
+            $error = 1;
+        }
+    }
+#   9. Is $order_id length lower than 32 char?
+    if(length($order_id) > 32){
+        $error_message.="Order id too long, max. 32 chars.\n";
+        $error = 1;
+    }
+# If there is an error, show the error message and exit:
+    if($error){
+        print "Folowwing error/s found:\n";
+        print $error_message;
+        exit;
+    }
 # ---------------------------------------------------------------------
 
 # Chapter 4: ----------------------------------------------------------
