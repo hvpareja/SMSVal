90618182be81654aa91a83ee5db635a42f5f4d03
Héctor Valverde <hvpareja@hotmail.com>
2012-09-29 14:05:13 +0200

    Variable declaration at head

--- sms.pl
+++ sms.pl
@@ -1,19 +1,38 @@
 
+# Chapter 0: ----------------------------------------------------------
+# Headers and dependences ---------------------------------------------
 
 # Chapter 1: ----------------------------------------------------------
 # Input variables -----------------------------------------------------
 #                                                                     #
 #                                                                     #
-# Mandatory variables                                                 #
- my $login;                                                           #      
- my $password;                                                        #  
- my $message_type;                                                    #
- my $message;                                                         #  
- my $recipient;                                                       #  
- my $sender;                                                          #  
+# Mandatory variables:                                                #
+ my $login;              # Login: User name for SMSTrend              #
+                                                                      #
+ my $password;           # Password: Password for SMSTrend account    #
+                                                                      #
+ my $message_type;       # Message Type: GS (without customized       #
+                         # sender) or GP (with customized sender)     #
+                                                                      #  
+ my $message;            # Message to send. Maximum 160 char per      #
+                         # message or maximum 1000 char for           #
+                         # concatenated messages. The following chars #
+                         # count as two chars: ^ { } \[ ~ ] | Û;      #
+                                                                      #  
+ my $recipient;          # Target phone number with country prefix:   #
+                         # +34610123456 or 0034610123456              #
+                                                                      #  
+ my $sender;             # If message_type = GP, then, you can        #  
+                         # customize the remitent by this way:        #  
+                         # 1. Phone number with 16 number max.        #
+                         # 2. Alphanumeric string wiht 11 char max.   #
+                                                                      #  
 # Optional variables                                                  #  
- my $sheduled_delivery_time;                                          #      
- my $order_id;                                                        #  
+ my $sheduled_delivery_time;  # Date and time to delivery the message #
+                              # Format: yyyyMMddHHmmss                #
+                                                                      #  
+ my $order_id;                # You can customize an unique order id  # 
+                              # to track the message: maximum 32 char #  
 #                                                                     #
 #                                                                     #
 # ---------------------------------------------------------------------
